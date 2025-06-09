6ae1a44bd77295c0f4d8456eedf49a80c602b7c1
FahadAli-Dev <214241178+FahadAli-Dev@users.noreply.github.com>
2025-06-10 00:42:06 +0500

    completed header section

--- src/Header/Header.jsx
+++ src/Header/Header.jsx
@@ -1,11 +1,23 @@
+import { BiMenuAltRight } from "react-icons/bi";
 import "./Header.css";
+import { useState } from "react";
 
 const Header = () => {
+  let [openMenu, setOpenMenu] = useState(false);
+
   return (
     <header className="h-wrapper">
       <nav className="h-container">
         <img src="logo.png" alt="logo" width={100} />
-        <div className="menu">
+        <BiMenuAltRight
+          className="menu-icon"
+          size={30}
+          color="white"
+          onClick={() => {
+            setOpenMenu(!openMenu);
+          }}
+        />
+        <div className={`menu ${openMenu ? "showMenu" : ""}`}>
           <a href="">Residencies</a>
           <a href="">Our Values</a>
           <a href="">Get Started</a>
