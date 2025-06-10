7289fa83336e6df03d7114948113932278d99b77
FahadAli-Dev <214241178+FahadAli-Dev@users.noreply.github.com>
2025-06-10 21:53:46 +0500

    completed header

--- src/Header/Header.jsx
+++ src/Header/Header.jsx
@@ -1,6 +1,7 @@
 import { BiMenuAltRight } from "react-icons/bi";
 import "./Header.css";
 import { useState } from "react";
+import OutsideClickHandler from "react-outside-click-handler";
 
 const Header = () => {
   let [openMenu, setOpenMenu] = useState(false);
@@ -9,21 +10,28 @@ const Header = () => {
     <header className="h-wrapper">
       <nav className="h-container">
         <img src="logo.png" alt="logo" width={100} />
-        <div
-          className="menu-icon"
-          onClick={() => {
-            setOpenMenu(!openMenu);
+
+        <OutsideClickHandler
+          onOutsideClick={() => {
+            setOpenMenu(false);
           }}
         >
-          <BiMenuAltRight size={30} color="white" />
-        </div>
+          <div className={`menu ${openMenu ? "showMenu" : ""}`}>
+            <a href="">Residencies</a>
+            <a href="">Our Values</a>
+            <a href="">Get Started</a>
+            <button>Contact Us</button>
+          </div>
 
-        <div className={`menu ${openMenu ? "showMenu" : ""}`}>
-          <a href="">Residencies</a>
-          <a href="">Our Values</a>
-          <a href="">Get Started</a>
-          <button>Contact Us</button>
-        </div>
+          <div
+            className="menu-icon"
+            onClick={() => {
+              setOpenMenu(!openMenu);
+            }}
+          >
+            <BiMenuAltRight size={30} color="white" />
+          </div>
+        </OutsideClickHandler>
       </nav>
     </header>
   );
